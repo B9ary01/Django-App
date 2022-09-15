88006d678208714f021a386e3432d885f38cc546
B9ary01 <bishalrai111@yahoo.com>
2022-09-15 10:34:52 +0100

    add artist model

--- songs/artists/models.py
+++ songs/artists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ArtistType(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    type=models.ForeignKey(ArtistType, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
